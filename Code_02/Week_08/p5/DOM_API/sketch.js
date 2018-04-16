cdaeb88a72a9df132536f8512077e5a8e4f01aa0
chenyizhou97 <chenyizhou97@users.noreply.github.com>
2018-04-16 12:11:01 -0400

    hmw week8

---
+++ Code_02/Week_08/p5/DOM_API/sketch.js
@@ -0,0 +1,47 @@
+var allRecipes= [];	// used to store all the breeds data from the API request
+var recipeSelectElement; // gives the user an option to select a breed
+var buttonElement; // gives the user a button to press after selecting the breed 
+var linkElement;	// the reference to the image element we'll be using to show the doggo
+var foodTyped; // variable storing the string to the currently selected breed
+
+function setup() {
+  noCanvas();
+
+  loadJSON('http://www.recipepuppy.com/api/', gotAllrecipes);
+
+  createElement('h1', 'Here is the recipe wiki');
+  createElement('h3', 'Try to find out the dish you want to make and see how to make it!');
+
+  buttonElement = createButton('Delicious!');
+  buttonElement.mousePressed(onButtonPressed);
+  var recipeSelectElement=createInput('');
+  inp.input(recipeSelectElement	);
+
+
+}
+
+function gotAllrecipes(data) {
+	allRecipes = Object.value(data.message);
+	for (var i = 0; i < allRecipes.length; i++) {
+		recipeSelectElement.option(allRecipes[i]);
+	}
+	selectedrecipe = recipeSelectElement.value();
+	recipeSelectElement.changed(searchEvent);
+}
+	
+// callback for changing the select element, on line 46
+function searchEvent() {
+	selectedrecipe = recipeSelectElement.value();
+	console.log(selectedrecipe);
+}
+
+// http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3 
+function onButtonPressed() {
+	loadJSON('http://www.recipepuppy.com/api/?q=' + selectedrecipe + '&p=1', onGotRecipes);
+}
+
+// callback for line 58, when the API request is completed, display the new image and delete the old one.
+function onGotRecipes(data) {
+	imgElement.remove();
+	imgElement = createImg(data.message);
+}
