9d103c6a453f51d793467b6c56c374af6024e503
chenyizhou97 <chenyizhou97@users.noreply.github.com>
2018-10-10 17:07:10 -0400

    1

---
+++ Core_Studio_System/Project_1/p5/empty-example/sketch.js
@@ -0,0 +1,27 @@
+var  s;
+var  h;
+var  me;
+var  school;
+var  home;
+var  i;
+var  r, g, b;
+
+
+
+function preload() {
+  s = loadImage("assets/school.png")
+  h = loadImage("assets/home.png")
+  me = loadImage("assets/me.png")
+}
+
+
+function setup() {
+  createCanvas(1300, 700);
+  rect(0,500,1300,200);
+  color(0);
+}
+
+
+function draw() {
+
+}
