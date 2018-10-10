730da590274c7244c26bba5aaae8eed546a644d6
chenyizhou97 <chenyizhou97@users.noreply.github.com>
2018-10-10 18:59:31 -0400

    11

--- Core_Studio_System/Project_1/p5/empty-example/sketch.js
+++ Core_Studio_System/Project_1/p5/empty-example/sketch.js
@@ -4,8 +4,10 @@ var  me;
 var  school;
 var  home;
 var  i;
-var  r, g, b;
-
+var  r = 9;
+var  b = 255;
+var g;
+var gspeed = false;
 
 
 function preload() {
@@ -17,11 +19,45 @@ function preload() {
 
 function setup() {
   createCanvas(1300, 700);
-  rect(0,500,1300,200);
-  color(0);
+  g = 255;
+  i = createSprite(200,288,85,212);
+  i.addImage(me);
+  home = createSprite(100,234,349,266);
+  home.addImage(h);
+  school = createSprite(864,161,336,339);
+  school.addImage(s);
+
 }
 
 
 function draw() {
+ if(!gspeed){
+  g--;
+ }
 
+if(gspeed){
+  g++;
+}
+
+
+background(r, g, b);
+
+
+    fill(150);
+  noStroke();
+  rect(0,500,1300,200);
+
+  drawSprite(school);
+  drawSprite(home);
+  drawSprite(i);
+
+}
+
+function mouseClicked(){
+   if(!gspeed){
+    gspeed = true;
+ }
+   else if(gspeed){
+    gspeed = false;
+   }
 }
