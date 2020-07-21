b582e4cf188053ed2c77733f748eb586620aeb5a
Yiezy <chenyizhou918@gmail.com>
2020-07-20 21:50:50 -0700

    Top funcition

--- Web_Dev/Pizza/app.js
+++ Web_Dev/Pizza/app.js
@@ -242,3 +242,8 @@ function stop_2() {
     US_map.stop();
     restAll();
 }
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
